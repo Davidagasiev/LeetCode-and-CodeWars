1a61f66ee5d59d367fdeabf25edab627802c3815
David Agasiev <62882376+Davidagasiev@users.noreply.github.com>
2020-06-23 12:45:02 +0400

    Solution

--- Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies.js
+++ Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies.js
@@ -1,7 +1,7 @@
 var kidsWithCandies = function(candies, extraCandies) {
-    var max = Math.max(...candies); //store the maximum value of all candies in a variable max
-    var res = []; //maintain a result array to store if that index can form the max value consuming all extraCandies
-    for(var i =0;i<candies.length;i++){
+    let max = Math.max(...candies), //store the maximum value of all candies in a variable max
+        res = []; //maintain a result array to store if that index can form the max value consuming all extraCandies
+    for(let i =0;i<candies.length;i++){
 	//check if ith index is greater than or equal to max
         if(candies[i]+extraCandies >= max){
 			//if so push true

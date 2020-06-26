79f25ecc16f447bb6427129e5bab4f7b09df5979
David Agasiev <62882376+Davidagasiev@users.noreply.github.com>
2020-06-27 00:44:01 +0400

    Solution

--- Valid Anagram/Valid Anagram.js
+++ Valid Anagram/Valid Anagram.js
@@ -1,16 +1,3 @@
-var isAnagram = (s, t) => {
-    let main1 = s.split("").sort(),
-    	main2 = t.split("").sort(),
-    	res = true;
-    	if (main1.length === main2.length) {
-    		main1.forEach( function(element, index) {
-    			if (element != main2[index]) {
-    			res = false;
-       			}
-    		});
-    	} else {
-    		res = false;
-    	}
-    
-    return res;
+var isAnagram = function(str1, str2) {
+	return str1.split("").sort().join("") === str2.split("").sort().join("") ? true : false;
 };

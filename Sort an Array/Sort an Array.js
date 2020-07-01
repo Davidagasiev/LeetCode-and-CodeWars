8f027a8b633d5cda12b759cf340c0cda25dd7ae9
David Agasiev <davidagasiev2005@gmail.com>
2020-07-01 17:46:36 +0400

    Given an array of integers nums, sort the array in ascending order.

---
+++ Sort an Array/Sort an Array.js
@@ -0,0 +1,16 @@
+var sortArray = function(nums) {
+    let noSwaps;
+    for(let i = nums.length - 1;i > 0;i--){
+    	noSwaps = true;
+    	for(let j = 0;j < i;j++){
+    		if (nums[j] > nums[j + 1]) {
+    			let temp = nums[j];
+    			nums[j] = nums[j + 1];
+    			nums[j + 1] = temp;
+    			noSwaps = false;
+    		}
+	   	}
+	   	if(noSwaps) break;
+    }
+    return nums;
+};
